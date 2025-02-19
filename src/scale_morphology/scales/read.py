0d2ee9c2a9ef9c0d5573c8a9df43fe54e71938a5
Richard Lane <richard.lane@bristol.ac.uk>
2025-02-19 13:39:54 +0000

    fcn for reading in data

---
+++ src/scale_morphology/scales/read.py
@@ -0,0 +1,40 @@
+"""
+Utilities for reading data, files, etc.
+
+"""
+
+import yaml
+import pathlib
+
+
+def _thisfile() -> pathlib.Path:
+    """
+    Get the path of this file.
+
+    """
+    return pathlib.Path(__file__)
+
+
+def _root() -> pathlib.Path:
+    """
+    Get the root directory of the project.
+
+    """
+    return _thisfile().parents[3]
+
+
+def config() -> dict:
+    """
+    Read the config file and return it as a dictionary.
+
+    """
+    with open(_root() / "config.yml") as file:
+        return yaml.safe_load(file)
+
+
+def data_dir() -> pathlib.Path:
+    """
+    Get the directory holding the data
+
+    """
+    return _root() / "data" / config()["binary_img_dir"]
