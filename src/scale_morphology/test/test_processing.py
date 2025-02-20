539b97b0e2f5cffdb69d4a30634167d0d0874165
Richard Lane <richard.lane@bristol.ac.uk>
2025-02-20 13:01:12 +0000

    test for holes

--- src/scale_morphology/test/test_processing.py
+++ src/scale_morphology/test/test_processing.py
@@ -1,5 +1,31 @@
+import numpy as np
+
 from ..scales import processing
 
 
 def test_has_holes():
-    ...
+    """
+    Check we can detect holes properly
+
+    """
+    img_with_holes = np.array(
+        [
+            [0, 0, 0, 0, 0],
+            [0, 1, 1, 1, 0],
+            [0, 1, 0, 1, 0],
+            [0, 1, 1, 1, 0],
+            [0, 0, 0, 0, 0],
+        ]
+    )
+    assert processing.has_holes(img_with_holes)
+
+    img_without_holes = np.array(
+        [
+            [0, 0, 0, 0, 0],
+            [0, 1, 1, 1, 0],
+            [0, 1, 1, 1, 0],
+            [0, 1, 1, 0, 0],
+            [0, 0, 0, 0, 0],
+        ]
+    )
+    assert not processing.has_holes(img_without_holes)
