b10c34f1463a753166cf0e569baa241f6494fb6c
Richard Lane <richard.lane@bristol.ac.uk>
2025-02-20 13:20:19 +0000

    fill bkg test + fcn

--- src/scale_morphology/scales/processing.py
+++ src/scale_morphology/scales/processing.py
@@ -12,14 +12,63 @@ def has_holes(binary_img: np.ndarray) -> bool:
     """
     Check if a binary image has holes.
 
+    :param binary_img: Binary image, with values 0 or 255 and dtype uint8
+    :return: bool
+
     """
+    assert np.isdtype(
+        binary_img.dtype, np.uint8
+    ), f"Input must be uint8: {binary_img.dtype=}"
+
     assert set(np.unique(binary_img)) <= {
         0,
-        1,
+        255,
     }, f"Input must be a binary image: {np.unique(binary_img)=}"
 
     # Find the number of disconnected background regions
     # If there's more than one, the image has a hole in it
-    _, num_features = ndimage.label(~binary_img.astype(bool))
+    _, num_features = ndimage.label(~binary_img)
 
     return num_features > 1
+
+
+def fill_background(binary_img: np.typing.NDArray) -> np.typing.NDArray:
+    """
+    Fill in the background of a binary image containing multiple disconnected background
+    regions. All but the largest region will be filled in.
+
+    :param binary_img: uint8 binary image, with values 0 or 255
+    :return: the image with all regions but the largest filled in
+
+    """
+    assert np.isdtype(
+        binary_img.dtype, np.uint8
+    ), f"Input must be uint8: {binary_img.dtype=}"
+
+    assert set(np.unique(binary_img)) <= {
+        0,
+        255,
+    }, f"Binary image must be 0 or 255: {np.unique(binary_img)}"
+
+    # Label distinct regions of background
+    background = ~binary_img
+    labels, num_features = ndimage.label(background)
+
+    # More than 1 region of bkg means we have holes
+    if num_features > 1:
+        copy = binary_img.copy()
+
+        # Create a list of bkg labels in descending order of size
+        counts = {
+            k: v for k, v in zip(*np.unique(labels, return_counts=True)) if k != 0
+        }
+
+        # Remove the pair from this dict with the highest value
+        counts = dict(sorted(counts.items(), key=lambda item: item[1], reverse=True))
+        counts = {k: v for k, v in counts.items() if k != list(counts.keys())[0]}
+
+        # Remove the holes
+        for k in counts.keys():
+            copy[labels == k] = 255
+
+    return copy
--- src/scale_morphology/test/test_processing.py
+++ src/scale_morphology/test/test_processing.py
@@ -1,31 +1,59 @@
+import pytest
 import numpy as np
 
 from ..scales import processing
 
 
-def test_has_holes():
-    """
-    Check we can detect holes properly
-
-    """
-    img_with_holes = np.array(
+@pytest.fixture
+def img_with_holes():
+    return 255 * np.array(
         [
             [0, 0, 0, 0, 0],
             [0, 1, 1, 1, 0],
             [0, 1, 0, 1, 0],
             [0, 1, 1, 1, 0],
             [0, 0, 0, 0, 0],
-        ]
+        ],
+        dtype=np.uint8,
     )
+
+
+def test_has_holes(img_with_holes: np.ndarray):
+    """
+    Check we can detect holes properly
+
+    """
     assert processing.has_holes(img_with_holes)
 
-    img_without_holes = np.array(
+    img_without_holes = 255 * np.array(
         [
             [0, 0, 0, 0, 0],
             [0, 1, 1, 1, 0],
             [0, 1, 1, 1, 0],
             [0, 1, 1, 0, 0],
             [0, 0, 0, 0, 0],
-        ]
+        ],
+        dtype=np.uint8,
     )
     assert not processing.has_holes(img_without_holes)
+
+
+def test_fill_bkg(img_with_holes: np.ndarray):
+    """
+    Check we fill the bkg regions correctly
+
+    """
+    assert np.all(
+        processing.fill_background(img_with_holes)
+        == 255
+        * np.array(
+            [
+                [0, 0, 0, 0, 0],
+                [0, 1, 1, 1, 0],
+                [0, 1, 1, 1, 0],
+                [0, 1, 1, 1, 0],
+                [0, 0, 0, 0, 0],
+            ],
+            dtype=np.uint8,
+        )
+    )
