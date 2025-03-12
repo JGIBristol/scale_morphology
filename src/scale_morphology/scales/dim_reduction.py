51db58927c0b64822c9c1c5c4daec5080dcc611f
Richard Lane <richard.lane@bristol.ac.uk>
2025-03-12 10:34:44 +0000

    dimensionality reduction lib

--- src/scale_morphology/scales/dim_reduction.py
+++ src/scale_morphology/scales/dim_reduction.py
@@ -4,21 +4,22 @@ Stuff for dimensionality reduction
 """
 
 import numpy as np
+from sklearn.decomposition import PCA
+from umap import UMAP
 
 
 def flatten(coeffs: np.typing.NDArray) -> np.typing.NDArray:
     """
-    Flatten the input data
-
-    :param coeffs: An array of coefficients representing the data
+    Flatten the input data to remove the harmonic dimension
     """
+    return coeffs.reshape((coeffs.shape[0], -1))
 
 
 def pca(coeffs: np.typing.NDArray, *, flatten: bool = False) -> np.typing.NDArray:
     """
     Perform PCA on the input data
 
-    :param scales: An array of coefficients representing the data.
+    :param coeffs: An array of coefficients representing the data.
                    May be shaped (N, d) if each image is represented by a single vector
                    or (N, d, 4), if each image is represented by a set of length-4 vectors,
                    as is the case for the EFA.
@@ -27,6 +28,33 @@ def pca(coeffs: np.typing.NDArray, *, flatten: bool = False) -> np.typing.NDArra
 
     :return: The transformed data, as a numpy array of shape (N, 2)
     """
-    # If flatten is True, flatten the data to the right shape
-    # Check the shape of data - should be 2d
-    # Perform PCA
+    if flatten:
+        coeffs = flatten(coeffs)
+
+    if coeffs.ndim != 2:
+        raise ValueError("Coeffs should be 2D; 3D input should be flattened")
+
+    return np.ascontiguousarray(PCA(n_components=2).fit_transform(coeffs))
+
+
+def umap(coeffs: np.typing.NDArray, *, flatten: bool = False) -> np.typing.NDArray:
+    """
+    Use UMAP for dimensionality reduction
+
+    :param coeffs: An array of coefficients representing the data.
+                   May be shaped (N, d) if each image is represented by a single vector
+                   or (N, d, 4), if each image is represented by a set of length-4 vectors,
+                   as is the case for the EFA.
+                   In the latter case the input should be flattened.
+    :param flatten: Whether to flatten the output
+
+    :return: The transformed data, as a numpy array of shape (N, 2)
+
+    """
+    if flatten:
+        coeffs = flatten(coeffs)
+
+    if coeffs.ndim != 2:
+        raise ValueError("Coeffs should be 2D; 3D input should be flattened")
+
+    return UMAP(n_components=2).fit_transform(coeffs)
---
+++ src/scale_morphology/test/test_dim_reduction.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+from ..scales import dim_reduction
+
+
+def test_flatten():
+    """
+    Check we get the right shape when we flatten an array of EFA coeffs
+
+    """
+    # 2 scales, 3 harmonics
+    efa_coeffs = np.array(
+        [
+            [
+                [1, 2, 3, 4],
+                [5, 6, 7, 8],
+                [9, 10, 11, 12],
+            ],
+            [
+                [2, 3, 4, 5],
+                [6, 7, 8, 9],
+                [10, 11, 12, 13],
+            ],
+        ]
+    )
+
+    flattened = dim_reduction.flatten(efa_coeffs)
+
+    assert flattened.shape == (2, 12)
+    assert (
+        flattened
+        == np.array(
+            [
+                [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+                [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+            ]
+        )
+    ).all()
