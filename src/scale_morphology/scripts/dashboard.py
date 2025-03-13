04fe32a064dfc5fd87a8afa8e7d4c9b0e55ab203
Richard Lane <richard.lane@bristol.ac.uk>
2025-03-13 14:59:32 +0000

    output in gitingore

---
+++ src/scale_morphology/scripts/dashboard.py
@@ -0,0 +1,23 @@
+"""
+Create a dashboard to visualise the dimensionality reduction
+of the EFA, autoencoder, or VAE coefficients.
+
+"""
+
+import argparse
+
+
+def main():
+    """
+    Read in the specified coefficients then create the dashboard
+    """
+
+
+def cli():
+    """
+    Command line interface
+    """
+
+
+if __name__ == "__main__":
+    cli()
