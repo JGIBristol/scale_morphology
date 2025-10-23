56de1d6ee9c2e2e121ae06bc283c56b0156029fd
Richard Lane <richard.lane@bristol.ac.uk>
2025-10-23 15:54:54 +0100

    a failing test that ensures rotated images give us the same coefficients...

--- src/scale_morphology/test/test_efd.py
+++ src/scale_morphology/test/test_efd.py
@@ -1,5 +1,11 @@
+"""
+Tests for the efa module
+
+"""
+
 import pyefd
 import numpy as np
+from skimage.morphology import disk
 
 from ..scales import efa
 
@@ -47,3 +53,36 @@ def test_rotation():
         pyefd.normalize_efd(coeffs.copy(), size_invariant=False),
         efa._rotate(coeffs),
     )
+
+
+def test_img_rotation():
+    """
+    Check that we get the same EFA coefficients for two images that are the same but rotated
+    """
+    # Generate both images
+    empty = np.zeros((32, 32))
+
+    r1, r2 = 4, 6
+    disc1 = disk(r1)
+    disc2 = disk(r2)
+
+    img1 = empty.copy()
+    x1, y1 = 10, 10
+    x2, y2 = 15, 18
+    img1[y1 - r1 : y1 + r1 + 1, x1 - r1 : x1 + r1 + 1] = disc1
+    img1[y2 - r2 : y2 + r2 + 1, x2 - r2 : x2 + r2 + 1] = np.maximum(
+        img1[y2 - r2 : y2 + r2 + 1, x2 - r2 : x2 + r2 + 1], disc2
+    )
+
+    img2 = np.rot90(img1, k=1)
+
+    img1 = (255 * img1).astype(np.uint8)
+    img2 = (255 * img2).astype(np.uint8)
+
+    # Get EFA coeffs for both
+    coeffs1 = efa.coefficients(img1, 50, 5)
+    coeffs2 = efa.coefficients(img2, 50, 5)
+
+    assert (
+        (coeffs1 == coeffs2).all()
+    ), "Currently broken because my rotation doesn't do it right?"
